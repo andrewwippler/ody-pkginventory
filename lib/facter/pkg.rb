15868a5ec0f2b3627820acae6e4ec395babfccd2
Andrew Wippler <andrew.wippler@gmail.com>
2016-04-19 16:29:50 -0700

    Adding attribution and Darwin Support

--- lib/facter/pkg.rb
+++ lib/facter/pkg.rb
@@ -1,4 +1,7 @@
 # Cody Herriges <cody@puppetlabs.com>
+# Johan Haals <johan.haals@gmail.com> (Darwin: https://github.com/jhaals/jhaals-app_inventory)
+# Andrew Wippler <andrew.wippler@gmail.com> (Windows)
+# Jonathan Dowland (Variable name fixes)
 #
 # Collects and creates a fact for every package installed on the system and
 # returns that package's version as the fact value.  Useful for doing package
@@ -8,7 +11,7 @@ require 'facter/util/pkg'
 
 Facter::Util::Pkg.package_list.each do |key, value|
   Facter.add(:"pkg_#{key}") do
-    confine :operatingsystem => %w{CentOS Fedora Redhat Debian Ubuntu Solaris windows}
+    confine :operatingsystem => %w{CentOS Fedora Redhat Debian Ubuntu Solaris windows Darwin}
     setcode do
       value
     end
--- lib/facter/util/pkg.rb
+++ lib/facter/util/pkg.rb
@@ -1,23 +1,75 @@
 # Cody Herriges <cody@puppetlabs.com>
+# Johan Haals <johan.haals@gmail.com> (Darwin: https://github.com/jhaals/jhaals-app_inventory)
+# Andrew Wippler <andrew.wippler@gmail.com> (Windows)
+# Jonathan Dowland (Variable name fixes)
 #
 # Collects and creates a fact for every package installed on the system and
 # returns that package's version as the fact value.  Useful for doing package
 # inventory and making decisions based on installed package versions.
+
+require 'rexml/document'
+include REXML
+
 module Facter::Util::Pkg
+  def self.validname(name)
+    name = name.tr('-+.','_') # RPM and deb packages might contain these
+    name = name.sub(/:.*/,'') # Debian multiarch arch qualifiers
+    name
+  end
+
+  def self.parse(element)
+    case element.name
+    when 'array'
+      element.elements.map {|child| parse(child)}
+    when 'dict'
+      result = {}
+      element.elements.each_slice(2) do |key, value|
+        result[key.text] = parse(value)
+      end
+      result
+    when 'real'
+      element.text.to_f
+    when 'integer'
+      element.text.to_i
+    when 'string', 'date'
+      element.text
+    end
+  end
+
   def self.package_list
     packages = []
-    case Facter.operatingsystem
+    case Facter.value(:operatingsystem)
     when 'Debian', 'Ubuntu'
       command = 'dpkg-query -W'
       packages = []
       Facter::Util::Resolution.exec(command).each_line do |pkg|
-        packages << pkg.chomp.split("\t")
+        name,version = pkg.chomp.split("\t")
+        packages << [Facter::Util::Pkg::validname(name),version]
       end
     when 'CentOS', 'RedHat', 'Fedora'
       command = 'rpm -qa --qf %{NAME}"\t"%{VERSION}-%{RELEASE}"\n"'
       packages = []
       Facter::Util::Resolution.exec(command).each_line do |pkg|
-        packages << pkg.chomp.split("\t")
+        name,version = pkg.chomp.split("\t")
+        packages << [Facter::Util::Pkg::validname(name),version]
+        end
+    when 'windows'
+      command = 'wmic product get name,version /format:csv'
+      packages = []
+      Facter::Util::Resolution.exec(command).each_line do |pkg|
+        packages << pkg.chomp.split(",").drop(1)
+      end
+    when 'Darwin'
+      xml = Facter::Util::Resolution.exec('system_profiler SPApplicationsDataType -xml')
+      xmldoc = Document.new(xml)
+      command = Facter::Util::Pkg::parse(xmldoc.root[1])
+      packages = []
+      command[0]['_items'].each do |pkg|
+        if pkg['path'].match(/^\/Applications/) and pkg['path'].index('Utilities') == nil
+          if !pkg['version'].nil?
+            packages << [pkg['_name'].downcase.gsub(' ',''),pkg['version']]
+          end
+        end
       end
     when 'Solaris'
       command = 'pkginfo -x'
@@ -35,13 +87,7 @@ module Facter::Util::Pkg
         packages << pkg.chomp.scan(/^(\S+).*\s(\d.*)/)[0]
       end
     end
-    when 'windows'
-      # csv format includes node name in first column.
-      command = 'wmic product get name,version /format:csv'
-      packages = []
-      Facter::Util::Resolution.exec(command).each_line do |pkg|
-        packages << pkg.chomp.split(",").drop(1)
-      end
     return packages
   end
 end
+
